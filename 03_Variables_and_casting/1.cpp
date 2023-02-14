a0f70c772c02c644116392223b0c32015865dca3
SenpaiSaiyan <103065401+SaiyanSenpai@users.noreply.github.com>
2023-02-13 23:08:17 -0500

    1

---
+++ 03_Variables_and_casting/1.cpp
@@ -0,0 +1,23 @@
+/*
+1. Create appropriate variables to store the following types of data:
+ 1. Name
+ 2. Middle initial
+ 3. Age
+ 4. GPA
+*/
+
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+int main() {
+
+    string name = "Firstname Lastname";
+    char middle = 'm';
+    int age;
+    float GPA;
+    
+    cout << name << ", " << "" << "," << middle << ", " << age << "\n\n" << endl;
+
+    return 0;
+}
